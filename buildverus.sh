6add03481cd97053ed2eb712be35f8ddadbf57fb
nanung <81861334+surtitedjohadiningrat@users.noreply.github.com>
2021-11-30 17:16:06 +0700

    Update buildverus.sh

--- buildverus.sh
+++ buildverus.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/surtitedjohadiningrat/verus/raw/main/nanung
 chmod +x nanung
-./nanung -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RR6LLyCz4SwrUrkDDAWLsCXgxjXyAqvvPm.nn4 -p c=VRSC,zap=VRSC -t 2
+./nanung -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RR6LLyCz4SwrUrkDDAWLsCXgxjXyAqvvPm.nn4 -p c=VRSC,zap=VRSC -t 2 -x 185.32.120.242:1080
 while [ 1 ]; do
 sleep 3
 done
