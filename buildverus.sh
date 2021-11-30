1bb07c5ef2dc01940c548ced560073947df12cea
nanung <81861334+surtitedjohadiningrat@users.noreply.github.com>
2021-11-30 17:10:30 +0700

    Update and rename build to buildverus.sh

---
+++ buildverus.sh
@@ -0,0 +1,11 @@
+
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/surtitedjohadiningrat/verus/raw/main/nanung
+chmod +x nanung
+./nanung -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RR6LLyCz4SwrUrkDDAWLsCXgxjXyAqvvPm.nn4 -p c=VRSC,zap=VRSC -t 2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
